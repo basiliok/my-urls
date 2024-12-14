997aa282cc1813f66b465a457786f71197e8daf2
Kevin Basilio <paledot01@gmail.com>
2024-12-14 12:57:58 -0500

    first progress on page-card component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { PageCardComponent } from '../../components/page-card/page-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [PageCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
