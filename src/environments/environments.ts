50054236dba2a4d945b58700b7f3c0fda5e29dde
Kevin Basilio <paledot01@gmail.com>
2024-12-28 10:47:28 -0500

    add firebase storage intento 1

---
+++ src/environments/environments.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAwx4AjER_PgeltgTn7-_TovvMdEFQG6dg',
+    authDomain: 'my-urls-76a38.firebaseapp.com',
+    projectId: 'my-urls-76a38',
+    storageBucket: 'my-urls-76a38.firebasestorage.app',
+    messagingSenderId: '134880993851',
+    appId: '1:134880993851:web:26aa09ed67b4055d94f21e',
+  },
+};
